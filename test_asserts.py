137624e5994d7b2b0a7a4147ddd1ad135709c198
Patil <Vijayalaxmi.G.Patil@emc.com>
2017-09-01 20:28:53 +0530

    pip, sys imports taken care of

--- test_asserts.py
+++ test_asserts.py
@@ -1,18 +1,21 @@
-if not xmlrunner in sys.modules:
-	pip.main(["install", "xmlrunner"])
-	
+import sys
+import pip
+
+if not "xmlrunner" in sys.modules:
+        pip.main(["install", "xmlrunner-1.7.7"])
+
 import xmlrunner
 
 def runner(output = "CI_test_output"):
-	return xmlrunner.XMLTestRunner(
-		output = output
-		)
-		
+        return xmlrunner.XMLTestRunner(
+                output = output
+                )
+
 
 def test_simpleAddition(self):
-	#asserts starts below		
-	assert(2 + 2 == 4, "Team we've got a problem")
-	assert(2 + 2 == 5, "All is well")
-	
+        #asserts starts below           
+        assert(2 + 2 == 4, "Team we've got a problem")
+        assert(2 + 2 == 5, "All is well")
+
 if __name__ == "__main__":
-	runner().run(test_simpleAddition)
+        runner().run(test_simpleAddition)
