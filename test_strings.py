73ebe0a780b7e350111349c64746731f77b71cbd
Patil <Vijayalaxmi.G.Patil@emc.com>
2017-09-02 17:47:38 +0530

    corrected the spaces

--- test_strings.py
+++ test_strings.py
@@ -7,19 +7,22 @@ if not "xmlrunner" in sys.modules:
 if not "unittest" in sys.modules:
     pip.main(["install", "unittest"])
 
+if not "shutil" in sys.modules:
+	pip.main(["install", "shutil"])
+
 import xmlrunner
 import unittest
+import shutil
 
 class TestStringMethods(unittest.TestCase):
-
     def test_upper(self):
-        self.assertEqual('foo'.upper(), 'fOO')
-		print("completed test_upper test")
+        self.assertEqual('foo'.upper(), 'fOO')  #Here FOO is replaced by foo for testing failure
+        print("completed test_upper test")
 
     def test_isupper(self):
         self.assertTrue('FOO'.isupper())
         self.assertFalse('Foo'.isupper())
-		print("completed test_isupper test")
+        print("completed test_isupper test")
 
     def test_split(self):
         s = 'hello world d'
@@ -27,6 +30,7 @@ class TestStringMethods(unittest.TestCase):
         # check that s.split fails when the separator is not a string
         with self.assertRaises(TypeError):
             s.split(2)
-
+       
 if __name__ == '__main__':
     unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
+	#shutil.rmtree('./test-reports')
